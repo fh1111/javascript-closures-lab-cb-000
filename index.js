6d5b2e88dd89be1003d84b936c0aebefcc3bd175
Fer Hernandez <fh.1111@gmail.com>
2019-02-07 02:10:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,3 +27,4 @@ function createAnimal(animalType) {
 
 var sharkCreator = createAnimal('Shark');
 var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
